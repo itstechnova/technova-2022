e479bf7bbffd162f8bb3f907bc14660b410a5efa
xhnsylk <ylkcode@users.noreply.github.com>
2022-05-30 10:59:47 -0400

    bold amazing

--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
         <img src={techNovaText} alt="TechNova" />
         <h1>University of Waterloo’s Women+ in Tech Hackathon</h1>
         <CountDown />
-        <h2>until we blast off with another amazing hackathon! </h2>
+        <h2>until we blast off with another <b>amazing</b> hackathon! </h2>
         <h4>To get TechNova’s latest update, please join our mailing list:</h4>
       </div>
     </div>
