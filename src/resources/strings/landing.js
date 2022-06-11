da2490428c24c6bc8d162c81fbc6e7ba65cba780
Amanda Tang <amandatang88888@gmail.com>
2022-06-11 16:38:11 -0400

    landing page with hacker applications

--- src/resources/strings/landing.js
+++ src/resources/strings/landing.js
@@ -1,6 +1,10 @@
 const landing = {
     mailingListLink: "https://forms.gle/oMH7qzFheBznyTSbA",
-    mailingListLabel: "Join our Mailing List"
+    mailingListLabel: "Join our Mailing List",
+    applyLink: "",
+    applyLabel: "Apply to be a Hacker",
+    websiteLink2021: "https://www.2021.itstechnova.org",
+    websiteLabel2021: "2021 Website"
 }
 
 export default landing;
--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -1,20 +1,45 @@
 import "./Landing.scss";
-import techNovaText from "../../resources/images/techNovaText.svg";
-import CountDown from "../../components/Countdown/Countdown";
 import Button from "../../components/Button/Button";
+
+import techNovaText from "../../resources/images/techNovaText2.svg";
+import calendar from "../../resources/images/icons/calendar.svg";
+import location from "../../resources/images/icons/location.svg";
 import landing from "../../resources/strings/landing";
+import mail from "../../resources/images/socials/mailFill.svg";
+import ig from "../../resources/images/socials/igFill.svg";
+import linkedin from "../../resources/images/socials/linkedinFill.svg";
+import facebook from "../../resources/images/socials/facebookFill.svg";
+import twitter from "../../resources/images/socials/twitterFill.svg";
+import socials from "../../resources/strings/socials";
 
 const Landing = () => {
   return (
     <div className="banner">
       <div className="container">
-        <img src={techNovaText} alt="TechNova" />
-        <h1>University of Waterloo’s Women+ in Tech Hackathon</h1>
-        <CountDown />
-        <h2>until hacker applications open for another <b>amazing</b> hackathon! </h2>
-        <h4>To get TechNova’s latest update, please join our mailing list:</h4>
-        <div className="mailingListButton">
-          <Button link={landing.mailingListLink} label={landing.mailingListLabel} />
+        <div className="technova-container">
+          <img className="technova-text" src={techNovaText} alt="TechNova" />
+          <h4 className="presents-text">presents</h4>
+        </div>
+        <h1 className="hack-text">Hack with Us</h1>
+        <h4 className="description">University of Waterloo's Women in Tech Hackathon</h4>
+        <div className="location-container">
+          <img className="icon" src={calendar} alt="icon" />
+          <h4 className="time-text">August 23-28th</h4>
+          <img className="icon" src={location} alt="icon" />
+          <h4>Hybrid @ Waterloo ON</h4>
+        </div>
+        <div className="apply-and-socials-container">
+          <Button link={landing.applyLink} label={landing.applyLabel} type={"button-2"}/>
+          <div className="links-container">
+            <div className="socials-row">
+                <img className="socials-icon" src={mail} link={socials.email} alt="icon" />
+                <img className="socials-icon" src={ig} link={socials.instagram} alt="icon" />
+                <img className="socials-icon" src={linkedin} link={socials.linkedin} alt="icon" />
+                <img className="socials-icon" src={facebook} link={socials.facebook} alt="icon" />
+                <img className="socials-icon" src={twitter} link={socials.twitter} alt="icon" />
+            </div>
+            <a className="link-2021" href={landing.websiteLink2021}>{landing.websiteLabel2021}</a>
+          </div>
         </div>
       </div>
     </div>
