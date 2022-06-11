ccb81d820e5d50dfc35bfbf509daeda2e9a07f8b
Amanda Tang <amandatang88888@gmail.com>
2022-06-11 16:46:09 -0400

    add application link

--- src/resources/strings/landing.js
+++ src/resources/strings/landing.js
@@ -1,7 +1,7 @@
 const landing = {
     mailingListLink: "https://forms.gle/oMH7qzFheBznyTSbA",
     mailingListLabel: "Join our Mailing List",
-    applyLink: "",
+    applyLink: "https://forms.gle/qU7SkWLsWYu8KWgNA",
     applyLabel: "Apply to be a Hacker",
     websiteLink2021: "https://www.2021.itstechnova.org",
     websiteLabel2021: "2021 Website"
