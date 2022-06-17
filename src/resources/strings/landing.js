e36b39204799e74418077f9e95a9a9c23dc3b159
xhnsylk <ylkcode@users.noreply.github.com>
2022-06-16 23:43:38 -0400

    init

--- src/resources/strings/landing.js
+++ src/resources/strings/landing.js
@@ -2,7 +2,7 @@ const landing = {
     mailingListLink: "https://forms.gle/oMH7qzFheBznyTSbA",
     mailingListLabel: "Join our Mailing List",
     applyLink: "https://forms.gle/qU7SkWLsWYu8KWgNA",
-    applyLabel: "Apply to be a Hacker",
+    applyLabel: "Apply Now!",
     websiteLink2021: "https://www.2021.itstechnova.org",
     websiteLabel2021: "2021 Website"
 }
