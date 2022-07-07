dfd5bf946e367539be104454c87babf7cf7fbf8b
xhnsylk <ylkcode@users.noreply.github.com>
2022-07-07 19:49:42 -0400

    revert changes in sectionwrapper

--- src/components/SectionWrapper/SectionWrapper.jsx
+++ src/components/SectionWrapper/SectionWrapper.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "./SectionWrapper.scss";
 
-const SectionWrapper = ({ children, id, paddingTop, backgroundImg }) => {
-  console.log("background:", backgroundImg);
-  return (
-    <div className="section-wrapper" id={id}>
-      <div
-        className="section-container"
-        style={{
-          paddingTop: paddingTop || "80px",
-          backgroundImage: `url('${backgroundImg}')` || "none",
-        }}
-      >
-        <div className="section-content">{children}</div>
-      </div>
-    </div>
-  );
+const SectionWrapper = ({ children, id, paddingTop }) => {
+    return (
+        <div className="section-wrapper" id={id}>
+            <div className="section-container" style={{paddingTop: paddingTop || "80px"}}>
+                <div className="section-content">{children}</div>
+            </div>
+            
+        </div>
+    );
 };
 
 export default SectionWrapper;
