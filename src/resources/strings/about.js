13c46d97fb5391af5ecb9b15821549c90009a63c
Amanda Tang <59590407+amanda849@users.noreply.github.com>
2022-07-10 01:56:43 -0400

    fix dates

--- src/resources/strings/about.js
+++ src/resources/strings/about.js
@@ -4,10 +4,10 @@ const about = {
     "TechNova’s mission is to create safe, inclusive and empowering spaces for women and non-binary individuals to start, grow and thrive in the technology industry. We value fostering an inclusive community, connecting students with career opportunities, and empowering hackers to create. TechNova aims to provide hackers with an end-to-end experience to help accelerate students’ journeys in tech, whether they are a beginner just starting off or a seasoned hacker looking for new challenges.",
   prepareCaption:
     "Technical workshops and community-building events to prepare you for the hackathon and your career in technology! ",
-  prepareDates: "Aug 23-26, 2021",
+  prepareDates: "Aug 23-26, 2022",
   hackCaption:
     "A 36-hour virtual hackathon to work with a team of dedicated individuals to design, create, and build - bringing your project idea to life!",
-  hackDates: "Aug 26-28, 2021",
+  hackDates: "Aug 26-28, 2022",
   connectCaption:
     "Tech fair to meet fellow hackers, make friends, and build bridges with industry professionals who want to connect with hackers like you!",
   connectDates: "Aug 23-28, 2022",
