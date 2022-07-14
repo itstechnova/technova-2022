8467e522368ebebde89a79c51f00b4418f71a9d9
xhnsylk <ylkcode@users.noreply.github.com>
2022-07-14 09:26:30 -0400

    style showcase cards

--- src/sections/Sponsor/ShowCase.jsx
+++ src/sections/Sponsor/ShowCase.jsx
@@ -1,9 +1,10 @@
 import Button from "../../components/Button/Button";
 
-import amazon from "../../resources/images/sponsors/amazon.png";
-import cockroach from "../../resources/images/sponsors/cockroach.png";
-import sps from "../../resources/images/sponsors/sps.png";
-import pagerduty from "../../resources/images/sponsors/pagerduty.png";
+import amazon from "../../resources/images/sponsors_2021/amazon.png";
+import cockroach from "../../resources/images/sponsors_2021/cockroach.png";
+import sps from "../../resources/images/sponsors_2021/sps.png";
+import pagerduty from "../../resources/images/sponsors_2021/pagerduty.png";
+import arrow from "../../resources/images/WiCSButton.svg";
 
 import "./ShowCase.scss";
 
@@ -16,36 +17,15 @@ var showcase = [
   },
   {
     quote:
-      "Cockroach Labs is a company building CockroachDB, a distributed SQL database that helps you build cloud-native apps that survive everything and thrive everywhere -- just like a cockroach!",
-    link: "https://www.cockroachlabs.com/careers/",
-    image: cockroach,
-  },
-  {
-    quote: (
-      <p>
-        SPS Commerce is the world’s leading retail network, connecting trading
-        partners around the globe to optimize supply chain operations for all
-        retail partners. We support data-driven partnerships with innovative
-        cloud technology, customer-obsessed service and accessible experts so
-        our customers can focus on what they do best. To date, more than 95,000
-        companies in retail, distribution, grocery and e-commerce have chosen
-        SPS as their retail network. SPS has achieved 82 consecutive quarters of
-        revenue growth and is headquartered in Minneapolis. For additional
-        information, contact SPS at 866-245-8100 or visit{" "}
-        <a style={{ fontWeight: "normal" }} href="https://www.spscommerce.com/">
-          www.spscommerce.com
-        </a>
-        .
-      </p>
-    ),
-    link: "https://jobs.spscommerce.com/",
-    image: sps,
+      "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
+    link: "https://www.pagerduty.com/careers/",
+    image: pagerduty,
   },
   {
     quote:
-      "PagerDuty is the digital operations management platform that empowers the right action, when seconds matter. For teams who build and run the digital systems that power business, PagerDuty is the best way to manage urgent, mission-critical work to keep operations running smoothly, without disruption of customer experiences. Over 40% of the Fortune 500 place PagerDuty at the center of their digital infrastructure to manage unplanned tasks, events, or opportunities. With more than 1,000 employees across offices in San Francisco, Atlanta, Toronto, Sydney, and London, PagerDuty is helping modern enterprises redefine their approach to operations in a digital-first world.",
-    link: "https://www.pagerduty.com/careers/",
-    image: pagerduty,
+      "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
+    link: "https://www.cockroachlabs.com/careers/",
+    image: cockroach,
   },
 ];
 
@@ -55,9 +35,16 @@ const ShowCase = () => {
       {showcase.map((sponsor, index) => {
         return (
           <div className="showcase-card">
-            <img className="sponsor-img" src={sponsor.image} />
-            <p>{sponsor.quote}</p>
-            <Button label="Explore career page" link={sponsor.link} />
+            <div className="showcase-content">
+              <img className="showcase-img" src={sponsor.image} />
+              <p className="showcase-quote">{sponsor.quote}</p>
+              <a className="showcase-button" href={sponsor.link}>
+                <div className="showcase-button-content">
+                  <p>Explore career page</p>
+                  <img src={arrow}></img>
+                </div>
+              </a>
+            </div>
           </div>
         );
       })}
