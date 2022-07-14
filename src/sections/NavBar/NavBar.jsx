87054130b3e92aaf30538cae741179836faa1414
Amanda Tang <amandatang88888@gmail.com>
2022-07-14 16:47:50 -0400

    remove mlh banner

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -53,9 +53,9 @@ const NavBar = ({ routes, logoRoute }) => {
                         </div>
                     </div>
                 </div>
-                <a className="mlh-banner" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" rel="noreferrer" target="_blank">
+                {/*<a className="mlh-banner" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" rel="noreferrer" target="_blank">
                     <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg" alt="Major League Hacking 20" />
-                </a>
+                </a>*/}
 
                 {showNav && <div className="nav-mobile-links">
                     {routes.map((route) => (
