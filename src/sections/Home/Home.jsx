997aa78558ce30f0b6e7d4972d75cff52cd05de1
Amanda Tang <59590407+amanda849@users.noreply.github.com>
2022-07-20 15:20:18 -0400

    Revert "Ticket #24 - Bug Fix: Add goose to left of Home Component"

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -3,35 +3,29 @@ import "./Home.scss";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import Button from "../../components/Button/Button";
 import landing from "../../resources/strings/landing";
-import goose from "../../resources/images/goose.png";
 
 const Home = () => {
   return (
     <SectionWrapper id="home" paddingTop="0">
-      <div className="content">
-        <div className="home-img">
-          <img src={goose} alt="goose" />
+      <div className="text">
+        <div className="h-block">
+          <h2>TechNova</h2>
+          <p>presents</p>
         </div>
-        <div className="text">
-          <div className="h-block">
-            <h2>TechNova</h2>
-            <p>presents</p>
-          </div>
-          <h1>Hack with Us</h1>
-          <p>University of Waterloo's Women+ in Tech Hackathon</p>
-          <p>August 23-28, 2022 • Virtual Event </p>
-          <div className="links">
-            <Button
-              link={landing.applyLink}
-              label={landing.applyLabel}
-              type={"button-2"}
-            />
-            <Button
-              link={landing.mentorLink}
-              label={landing.mentorLabel}
-              type={"button-2"}
-            />
-          </div>
+        <h1>Hack with Us</h1>
+        <p>University of Waterloo's Women+ in Tech Hackathon</p>
+        <p>August 23-28, 2022 • Virtual Event </p>
+        <div className="links">
+          <Button
+            link={landing.applyLink}
+            label={landing.applyLabel}
+            type={"button-2"}
+          />
+          <Button
+            link={landing.mentorLink}
+            label={landing.mentorLabel}
+            type={"button-2"}
+          />
         </div>
       </div>
     </SectionWrapper>
