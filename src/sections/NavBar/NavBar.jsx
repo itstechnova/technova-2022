92f25a6b2f8084b778449572f532906831a51df2
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-07-24 18:09:09 -0400

    removed test

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -10,75 +10,74 @@ const NavBar = ({ routes, logoRoute }) => {
 
   return (
     <nav>
-      <div className="test">
-        <div className="nav-container">
-          <div className="nav-content">
-            <div className="nav-links-container">
+      <div className="nav-container">
+        <div className="nav-content">
+          <div className="nav-links-container">
+            <Link
+              className="logo-link"
+              activeClass="active"
+              to={`/#${logoRoute.sectionId}`}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <img src={logoRoute.logoSrc} alt="logo" />
+            </Link>
+            <div className="nav-links">
+              <div className="nav-desktop-links">
+                {routes.map((route) => (
+                  <Link
+                    className="nav-link"
+                    activeClass="active"
+                    to={`/#${route.sectionId}`}
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                  >
+                    {route.label}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mobile">
+            {/*<ModeToggle className="toggle" />*/}
+            <div className="mobile-menu" onClick={() => setShowNav(!showNav)}>
+              {showNav ? (
+                <img className="nav-x" src={cross} alt="cross" />
+              ) : (
+                <img
+                  className="nav-hamburger"
+                  src={hamburger}
+                  alt="hamburger menu"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        {/*<a className="mlh-banner" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" rel="noreferrer" target="_blank">
+                    <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg" alt="Major League Hacking 20" />
+                </a>*/}
+
+        {showNav && (
+          <div className="nav-mobile-links">
+            {routes.map((route) => (
               <Link
-                className="logo-link"
+                className="nav-link"
                 activeClass="active"
-                to={`/#${logoRoute.sectionId}`}
+                to={`/#${route.sectionId}`}
                 spy={true}
                 smooth={true}
                 offset={-70}
                 duration={500}
+                onClick={() => setShowNav(false)}
               >
-                <img src={logoRoute.logoSrc} alt="logo" />
+                {route.label}
               </Link>
-              <div className="nav-links">
-                <div className="nav-desktop-links">
-                  {routes.map((route) => (
-                    <Link
-                      className="nav-link"
-                      activeClass="active"
-                      to={`/#${route.sectionId}`}
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={500}
-                    >
-                      {route.label}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="mobile">
-              {/*<ModeToggle className="toggle" />*/}
-              <div className="mobile-menu" onClick={() => setShowNav(!showNav)}>
-                {showNav ? (
-                  <img className="nav-x" src={cross} alt="cross" />
-                ) : (
-                  <img
-                    className="nav-hamburger"
-                    src={hamburger}
-                    alt="hamburger menu"
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-          {/*<a className="mlh-banner" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" rel="noreferrer" target="_blank">
-                    <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg" alt="Major League Hacking 20" />
-                </a>*/}
-
-          {showNav && (
-            <div className="nav-mobile-links">
-              {routes.map((route) => (
-                <Link
-                  className="nav-link"
-                  activeClass="active"
-                  to={`/#${route.sectionId}`}
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  onClick={() => setShowNav(false)}
-                >
-                  {route.label}
-                </Link>
-              ))}
-              {/*<div style={{ display: "flex", justifyContent: "center", width: "100%", marginTop: "20px"}}>
+            ))}
+            {/*<div style={{ display: "flex", justifyContent: "center", width: "100%", marginTop: "20px"}}>
                         <a
                             href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
                             rel="noreferrer" target="_blank"
@@ -88,9 +87,8 @@ const NavBar = ({ routes, logoRoute }) => {
                             official member
                         </a>
                     </div>*/}
-            </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </nav>
   );
