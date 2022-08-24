9d8ddeb0a8210d951e5d74c24e48434d5b892470
Amanda Tang <amandatang88888@gmail.com>
2022-08-23 22:12:34 -0400

    new info

--- src/sections/Team/Team.jsx
+++ src/sections/Team/Team.jsx
@@ -3,7 +3,8 @@ import Slider from "react-slick";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 
 import "./Team.scss";
-import alenka from "../../resources/images/team/alenka.png";
+import alenka from "../../resources/images/team/alenka.jpg";
+import alenka2 from "../../resources/images/team/alenka2.jpg";
 import amandag from "../../resources/images/team/amandag.jpeg";
 import amandag2 from "../../resources/images/team/amandag2.jpg";
 import amandat from "../../resources/images/team/amandat.jpg";
@@ -28,7 +29,8 @@ import elizabeth from "../../resources/images/team/elizabeth.jpg";
 import elizabeth2 from "../../resources/images/team/elizabeth2.jpg";
 import emily from "../../resources/images/team/emily.jpg";
 import emily2 from "../../resources/images/team/emily2.jpg";
-import fariha from "../../resources/images/team/fariha.png";
+import fariha from "../../resources/images/team/fariha.jpeg";
+import fariha2 from "../../resources/images/team/fariha2.jpg";
 import grace from "../../resources/images/team/grace.png";
 import jacquelyn from "../../resources/images/team/jacquelyn.png";
 import joyce from "../../resources/images/team/joyce.jpeg";
@@ -40,6 +42,7 @@ import kriti from "../../resources/images/team/kriti.jpg";
 import kun from "../../resources/images/team/kun.jpg";
 import kun2 from "../../resources/images/team/kun2.jpg";
 import lucy from "../../resources/images/team/lucy.png";
+import lucy2 from "../../resources/images/team/lucy2.png";
 import rachel from "../../resources/images/team/rachel.png";
 import rachel2 from "../../resources/images/team/rachel2.png";
 import samantha from "../../resources/images/team/samantha.jpg";
@@ -56,12 +59,12 @@ import SocialMediaLink from "../../components/SocialMediaLink/SocialMediaLink";
 import linkedin from "../../resources/images/socials/linkedinTeam.svg";
 
 const photos = [
-    { hot: lucy, goof: goose, name: "Lucy Shen", role: "Director", linkedin: "https:/www.linkedin.com/in/lucy-shen/" },
+    { hot: lucy, goof: lucy2, name: "Lucy Shen", role: "Director", linkedin: "https:/www.linkedin.com/in/lucy-shen/" },
     { hot: ayla, goof: ayla2, name: "Ayla Orucevic", role: "Advisor", linkedin: "https:/www.linkedin.com/in/ayla/" },
     
     { hot: samantha, goof: samantha2, name: "Samantha Kaiser", role: "Finance Lead", linkedin: "https:/www.linkedin.com/in/samanthakaiser03/" },
     { hot: amandag, goof: amandag2, name: "Amanda Gressmann", role: "Finance Coordinator", linkedin: "" },
-    { hot: joyce, goof: joyce2, name: "Joyce Jorda", role: "Finance Coordinator", linkedin: "" },
+    { hot: joyce, goof: joyce2, name: "Joyce Jorda", role: "Finance Coordinator", linkedin: "www.linkedin.com/in/joyce-jorda" },
     { hot: goose, goof: goose, name: "Derya Gulsoy", role: "Finance Coordinator & Sponsorship Coordinator", LinkedIn: "" },
 
     { hot: bernie, goof: bernie2, name: "Bernadette Cheng", role: "Sponsorship Coordinator", linkedin: "https:/www.linkedin.com/in/bernadettecheng/" },
@@ -72,14 +75,14 @@ const photos = [
     { hot: chidi, goof: chidi2, name: "Chidinma Umenwofor-Nweze", role: "Logistics Lead", LinkedIn: "www.linkedin.com/in/chidinma-umenwofor-nweze-b92990195/" },
     { hot: dea, goof: dea2, name: "Dea Gjini", role: "Logistics Coordinator", linkedin: "https:/www.linkedin.com/in/dea-gjini/" },
     { hot: julianne, goof: julianne2, name: "Julianne Jorda", role: "Logistics Coordinator", LinkedIn: "www.linkedin.com/in/julianne-jorda" },
-    { hot: angela, goof: angela2, name: "Angela Law", role: "Logistics Coordinator", LinkedIn: "" },
+    { hot: angela, goof: angela2, name: "Angela Law", role: "Logistics Coordinator", LinkedIn: "https://www.linkedin.com/in/angela-law/" },
     { hot: emily, goof: emily2, name: "Emily Zhu", role: "Logistics Coordinator", LinkedIn: "www.linkedin.com/in/emilyhy-zhu" },
     { hot: goose, goof: goose, name: "Maggie Lin", role: "Logistics Coordinator", LinkedIn: "" },
     { hot: shihan, goof: shihan2, name: "Shi Han Qin", role: "Logistics Coordinator", LinkedIn: "" },
     { hot: cindy, goof: goose, name: "Cindy Gu", role: "Logistics Coordinator", LinkedIn: "" },
 
     { hot: elizabeth, goof: elizabeth2, name: "Elizabeth Tatar", role: "Marketing Lead", LinkedIn: "" },
-    { hot: alenka, goof: goose, name: "Alenka Rondon", role: "Marketing Coordinator", LinkedIn: "" },
+    { hot: alenka, goof: alenka2, name: "Alenka Rondon", role: "Marketing Coordinator", LinkedIn: "https://www.linkedin.com/in/alenka-rondon/" },
     { hot: jacquelyn, goof: goose, name: "Jacquelyn Yu", role: "Marketing Coordinator", LinkedIn: "" },
     { hot: serena, goof: goose, name: "Serena Li", role: "Marketing Coordinator", LinkedIn: "www.linkedin.com/in/serena-li-03/" },
     
@@ -90,7 +93,7 @@ const photos = [
     { hot: catherine, goof: catherine2, name: "Catherine Hoang", role: "Design Coordinator", LinkedIn: "www.linkedin.com/in/cat-hoang/" },
 
     { hot: amandat, goof: amandat2, name: "Amanda Tang", role: "Engineering Lead", LinkedIn: "www.linkedin.com/in/amanda-tang-509a7119b/" },
-    { hot: fariha, goof: goose, name: "Mahzabin Fariha", role: "Engineering Coordinator", LinkedIn: "" },
+    { hot: fariha, goof: fariha2, name: "Mahzabin Fariha", role: "Engineering Coordinator", LinkedIn: "https://www.linkedin.com/in/mahzabin-rashid/" },
     { hot: kelly, goof: goose, name: "Kelly Wu", role: "Engineering Coordinator", LinkedIn: "" },
 ]
 
